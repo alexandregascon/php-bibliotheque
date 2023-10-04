d5729148b8cb9ef73fa85740835b8e18db1c3ac0
Alexandre Gascon <alexandregascon@sfr.fr>
2023-10-04 09:46:48 +0200

    Premier tesy avec Php_unit

--- tests/ClasseATest.php
+++ tests/ClasseATest.php
@@ -8,14 +8,17 @@ class ClasseATest extends TestCase
     /**
      * @test
      */
-    public function fonctionne() {
-        $this->assertEquals(1,1);
+    public function DateAdhesion_DateDuJourSiNonPrecise_DateDuJour() {
+        $adherent = new \App\Adherents("Pierre","Gasly","");
+        $date = new DateTime();
+        $date = $date->format("d/m/Y");
+        $this->assertEquals($date,$adherent->getDateAdhesion()->format("d/m/Y"));
     }
 
-    /**
-     * @test
-     */
-    public function fonctionne_2() {
-        $this->assertEquals(1,1);
-    }
+//    /**
+//     * @test
+//     */
+//    public function fonctionne_2() {
+//        $this->assertEquals(1,1);
+//    }
 }
